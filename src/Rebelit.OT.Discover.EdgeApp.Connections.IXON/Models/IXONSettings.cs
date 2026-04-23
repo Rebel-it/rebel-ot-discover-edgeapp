eff266dbf55dc96ec687413ba0da19d967299a2e
LarsRebelIt <145104388+LarsRebelIt@users.noreply.github.com>
2026-04-23 11:26:10 +0200

    Feature/#54 edge app env variables api endpoint (#69)

---
+++ src/Rebelit.OT.Discover.EdgeApp.Connections.IXON/Models/IXONSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rebelit.OT.Discover.EdgeApp.Connections.IXON.Models;
+
+public class IXONSettings
+{
+    [Required]
+    public required string ApplicationId { get; set; }
+    [Required]
+    public required string CompanyId { get; set; }
+    [Required]
+    public required string AgentId { get; set; }
+    public string? DataSourceId { get; set; }
+}
