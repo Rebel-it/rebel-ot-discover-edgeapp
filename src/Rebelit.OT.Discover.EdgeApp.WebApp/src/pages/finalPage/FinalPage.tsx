4186a690f6e92533fc620d35659cd50cd4a29370
rubenrebelit <ruben@rebelit.nl>
2026-05-20 15:18:24 +0200

    Update FinalPage.tsx

--- src/Rebelit.OT.Discover.EdgeApp.WebApp/src/pages/finalPage/FinalPage.tsx
+++ src/Rebelit.OT.Discover.EdgeApp.WebApp/src/pages/finalPage/FinalPage.tsx
@@ -5,7 +5,7 @@ function FinalPage() {
   const navigate = useNavigate();
 
   return (
-    <div className={styles.loginWrapper}>
+    <div className={styles.wrapper}>
       <div>
         <h1>Completed!</h1>
         <p>You have successfully completed the setup. If you wish, you can run the wizard again
@@ -13,8 +13,6 @@ function FinalPage() {
           Finally, you can run the provided <code>remove_discover_edgeapp.sh</code> script to uninstall the DiscoverEdgeApp on your Secure Edge Pro.
         </p>
       </div>
-
-
 
       <button type="button" className={styles.nextButton} onClick={() => navigate('/')}>
         Finish
