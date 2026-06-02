5e655604952ea068a156f43abb93cffdc3b0a51c
rubenrebelit <ruben@rebelit.nl>
2026-06-02 13:57:34 +0200

    styling of startpage content

--- src/Rebelit.OT.Discover.EdgeApp.WebApp/src/pages/startPage/StartPage.tsx
+++ src/Rebelit.OT.Discover.EdgeApp.WebApp/src/pages/startPage/StartPage.tsx
@@ -5,6 +5,7 @@ import { WizardStep } from '../../models/WizardStep'
 
 function StartPage() {
   const navigate = useNavigate()
+  const description = "With the [OPC & Discovery App], you can easily connect your machines to the cloud using your IXON device. This enables you to remotely monitor your factory and keep full visibility over your operations.\n\nTo get started, we’ll guide you through a simple setup wizard. In just 7 steps, you’ll configure your IXON device. The process takes approximately [X] minutes.\n\nNote: you need to be connected to your machine via VPN."
 
   return (
     <WizardPage 
@@ -13,10 +14,14 @@ function StartPage() {
       onContinue={() => navigate('/login')}
     >
       <section className={styles.page}>
-          <h1 className={styles.title}>EdgeApp</h1>
+          <h1 className={styles.title}>[OPC & Discovery app]</h1>
           <p className={styles.description}>
-            Rebel:it EdgeApp helps you connect, configure, and manage edge devices so operators can
-            quickly start discovery workflows and keep the environment under control.
+            {description.split('\n\n').map((paragraph, index) => (
+              <span key={index}>
+                {paragraph}
+                <br /><br />
+              </span>
+            ))}
           </p>
       </section>
     </WizardPage>
