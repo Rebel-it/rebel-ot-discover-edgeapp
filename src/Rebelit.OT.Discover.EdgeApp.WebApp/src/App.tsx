622c4b5deb05d798f39b5527021e9301e3ac2e84
rubenrebelit <ruben@rebelit.nl>
2026-06-02 11:20:17 +0200

    basic css layout structure setup

--- src/Rebelit.OT.Discover.EdgeApp.WebApp/src/App.tsx
+++ src/Rebelit.OT.Discover.EdgeApp.WebApp/src/App.tsx
@@ -8,24 +8,27 @@ import SourcePage from './pages/sourcePage/SourcePage'
 import DeviceConfigPage from './pages/deviceConfigPage/DeviceConfigPage'
 import VariablesPage from './pages/variablesPage/VariablesPage'
 import FinalPage from './pages/finalPage/FinalPage'
+import Layout from './pages/layout/Layout'
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/final" element={<FinalPage />} />
-        <Route element={<ProtectedRoute />}>
+    <Layout >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/final" element={<FinalPage />} />
+          {/* <Route element={<ProtectedRoute />}> */}
           <Route path="/plc" element={<PlcConnect />} />
           <Route path="/source" element={<SourcePage />} />
           <Route path="/variables" element={<VariablesPage />} />
           <Route path="/tags" element={<TagPage />} />
           <Route path="/deviceconfig" element={<DeviceConfigPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
+    </Layout>
   )
 }
 
