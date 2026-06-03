6aafa1eaca1c9a46ededec7bd8c905d667a96161
rubenrebelit <ruben@rebelit.nl>
2026-06-03 14:20:27 +0200

    basic form field styling

--- src/Rebelit.OT.Discover.EdgeApp.WebApp/src/components/atoms/formField/FormField.tsx
+++ src/Rebelit.OT.Discover.EdgeApp.WebApp/src/components/atoms/formField/FormField.tsx
@@ -1,30 +1,30 @@
 import type { ComponentProps } from 'react'
 import styles from './FormField.module.css'
-type FormFieldProps = {
-    id: string
-    label: string
-    value: string
-    onChange: (value: string) => void
-    type?: ComponentProps<'input'>['type']
-    required?: boolean
-    autoComplete?: ComponentProps<'input'>['autoComplete']
+
+type Props = {
+  id: string
+  label: string
+  value: string
+  onChange: (value: string) => void
+  type?: ComponentProps<'input'>['type']
+  required?: boolean
 }
 
-function FormField({ id, label, value, onChange, type = 'text', required, autoComplete = 'off' }: Readonly<FormFieldProps>) {
-    return (
-        <div className={styles.formField}>
-            <label htmlFor={id}>{label}</label>
-            <input
-                id={id}
-                type={type}
-                value={value}
-                inputMode={type === 'password' ? undefined : 'text'}
-                onChange={(e) => onChange(e.target.value)}
-                required={required}
-                autoComplete={autoComplete}
-            />
-        </div>
-    )
+function FormField({ id, label, value, onChange,
+  type = 'text', required }: Readonly<Props>) {
+  return (
+    <div className={styles.formField}>
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        type={type}
+        value={value}
+        inputMode={type === 'password' ? undefined : 'text'}
+        onChange={(e) => onChange(e.target.value)}
+        required={required}
+      />
+    </div>
+  )
 }
 
-export default FormField
+export default FormField;
