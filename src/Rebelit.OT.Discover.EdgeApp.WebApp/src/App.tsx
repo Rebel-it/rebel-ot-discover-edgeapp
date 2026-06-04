7849b783d3e6559637636983c081c32ed254ae71
rubenrebelit <ruben@rebelit.nl>
2026-06-04 13:00:13 +0200

    Update App.tsx

--- src/Rebelit.OT.Discover.EdgeApp.WebApp/src/App.tsx
+++ src/Rebelit.OT.Discover.EdgeApp.WebApp/src/App.tsx
@@ -10,43 +10,45 @@ import FinalPage from './pages/finalPage/FinalPage'
 import { WizardProvider } from './context/WizardContext'
 import { Pages } from './models/Pages'
 import WizardStepGuard from './components/shared/WizardStepGuard'
-
+import { TagsProvider } from './context/TagContext'
 
 function App() {
   return (
     <WizardProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path={Pages.start} element={<StartPage />} />
-          <Route path={Pages.login} element={<LoginPage />} />
+      <TagsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path={Pages.start} element={<StartPage />} />
+            <Route path={Pages.login} element={<LoginPage />} />
 
-          <Route element={<WizardStepGuard requiredStep="login" />}>
-            <Route path={Pages.plcConnect} element={<PlcConnect />} />
-          </Route>
+            <Route element={<WizardStepGuard requiredStep="login" />}>
+              <Route path={Pages.plcConnect} element={<PlcConnect />} />
+            </Route>
 
-          <Route element={<WizardStepGuard requiredStep="plcConnect" />}>
-            <Route path={Pages.source} element={<SourcePage />} />
-          </Route>
+            <Route element={<WizardStepGuard requiredStep="plcConnect" />}>
+              <Route path={Pages.source} element={<SourcePage />} />
+            </Route>
 
-          <Route element={<WizardStepGuard requiredStep="source" />}>
-            <Route path={Pages.variables} element={<VariablesPage />} />
-          </Route>
+            <Route element={<WizardStepGuard requiredStep="source" />}>
+              <Route path={Pages.variables} element={<VariablesPage />} />
+            </Route>
 
-          <Route element={<WizardStepGuard requiredStep="variables" />}>
-            <Route path={Pages.tags} element={<TagPage />} />
-          </Route>
+            <Route element={<WizardStepGuard requiredStep="variables" />}>
+              <Route path={Pages.tags} element={<TagPage />} />
+            </Route>
 
-          <Route element={<WizardStepGuard requiredStep="tags" />}>
-            <Route path={Pages.deviceConfig} element={<DeviceConfigPage />} />
-          </Route>
+            <Route element={<WizardStepGuard requiredStep="tags" />}>
+              <Route path={Pages.deviceConfig} element={<DeviceConfigPage />} />
+            </Route>
 
-          <Route element={<WizardStepGuard requiredStep="deviceConfig" />}>
-            <Route path={Pages.final} element={<FinalPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+            <Route element={<WizardStepGuard requiredStep="deviceConfig" />}>
+              <Route path={Pages.final} element={<FinalPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </TagsProvider>
     </WizardProvider>
   )
 }
 
-export default App
+export default App;
