2078fa7599772ceec50f9a77ea383f5fce56e3ce
rubenrebelit <ruben@rebelit.nl>
2026-06-04 09:07:53 +0200

    rename

---
+++ src/Rebelit.OT.Discover.EdgeApp.WebApp/src/components/organisms/progressPanel/ProgressPanel.tsx
@@ -0,0 +1,37 @@
+import { useWizard } from "../../../context/WizardContext";
+import { WizardStep, WizardStepOrder, type WizardStepKey } from "../../../models/WizardStep";
+import WizardStepComponent from "../../molecules/wizardStepComponent/WizardStepComponent";
+import styles from "./ProgressPanel.module.css";
+
+type Props = {
+  currentStep?: WizardStepKey;
+}
+
+export default function ProgressPanel({ currentStep }: Readonly<Props>) {
+  const { isStepCompleted } = useWizard();
+
+  function isCurrentStep(stepKey: WizardStepKey): boolean {
+    return stepKey === currentStep;
+  }
+  
+  return (
+    <div className={styles.progressPanel}>
+      {WizardStepOrder.map((key, index) => {
+        const step = WizardStep[key];
+        const status = isStepCompleted(key) ? 'done'
+          : key === currentStep ? 'active'
+          : 'todo';
+
+        return (
+          <WizardStepComponent
+            key={key}
+            stepNumber={index + 1}
+            wizardStep={step}
+            status={status}
+            isCurrentStep={isCurrentStep(key)}
+          />
+        );
+      })}
+    </div>
+  );
+}
