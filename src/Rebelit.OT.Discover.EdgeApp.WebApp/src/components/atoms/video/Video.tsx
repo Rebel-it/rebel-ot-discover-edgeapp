3c7fd9da48eb6f6e2ff296ede0f5c40607ad4f0c
rubenrebelit <ruben@rebelit.nl>
2026-06-10 11:22:24 +0200

    Use service account tutorial video
    Add serviceaccounttutorial.mp4 asset and switch Video and VideoPreview to use it. Also remove an unused openvideoicon.svg import in VideoPreview. This replaces the displayed preview and modal video with the service account tutorial asset.

--- src/Rebelit.OT.Discover.EdgeApp.WebApp/src/components/atoms/video/Video.tsx
+++ src/Rebelit.OT.Discover.EdgeApp.WebApp/src/components/atoms/video/Video.tsx
@@ -1,5 +1,5 @@
 import styles from "./Video.module.css";
-import rebelVideo from "../../../assets/rebelVideo.mp4";
+import serviceAccountTutorial from "../../../assets/serviceaccounttutorial.mp4";
 import OpenVideoButton from "../openVideoButton/OpenVideoButton";
 
 type Props = {
@@ -10,7 +10,7 @@ export default function Video({ onClose }: Readonly<Props>) {
   return (
     <div className={styles.videoWrapper}>
       <video className={styles.video} autoPlay loop muted>
-        <source src={rebelVideo} type="video/mp4" />
+        <source src={serviceAccountTutorial} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <OpenVideoButton onClick={onClose} />
--- src/Rebelit.OT.Discover.EdgeApp.WebApp/src/components/atoms/videoPreview/VideoPreview.tsx
+++ src/Rebelit.OT.Discover.EdgeApp.WebApp/src/components/atoms/videoPreview/VideoPreview.tsx
@@ -1,6 +1,5 @@
 import styles from "./VideoPreview.module.css";
-import rebelVideo from "../../../assets/rebelVideo.mp4";
-import openVideoIcon from "../../../assets/openvideoicon.svg";
+import serviceAccountTutorial from "../../../assets/serviceaccounttutorial.mp4";
 import OpenVideoButton from "../openVideoButton/OpenVideoButton";
 
 type Props = {
@@ -11,7 +10,7 @@ export default function VideoPreview({ onClick }: Readonly<Props>) {
   return (
     <button className={styles.preview} onClick={onClick} aria-label="Open video">
       <video className={styles.video} autoPlay loop muted>
-        <source src={rebelVideo} type="video/mp4" />
+        <source src={serviceAccountTutorial} type="video/mp4" />
       </video>
       <div className={styles.overlay}>
         <OpenVideoButton />
