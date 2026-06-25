0f34c556f1b63d32b0df97928e340559b3c49e58
rubenrebelit <ruben@rebelit.nl>
2026-06-25 08:58:34 +0200

    Rich text renderer now uses react markdown

--- src/Rebelit.OT.Discover.EdgeApp.WebApp/src/components/atoms/richTextRenderer/RichTextRenderer.tsx
+++ src/Rebelit.OT.Discover.EdgeApp.WebApp/src/components/atoms/richTextRenderer/RichTextRenderer.tsx
@@ -1,38 +1,18 @@
+import Markdown from 'react-markdown';
+import type { Components } from 'react-markdown';
 import styles from './RichTextRenderer.module.css';
-
-const INLINE_SPLIT_REGEX = /(\*\*[^*]+\*\*|\[[^\]]+\]\(https?:\/\/[^)]+\)|https?:\/\/[^\s]+)/g;
 
 type Props = {
   text: string;
 }
 
-function renderInlinePart(part: string, key: number) {
-  if (part.startsWith('**') && part.endsWith('**')) {
-    return <strong key={key}>{part.slice(2, -2)}</strong>;
-  }
-  const markdownMatch = part.match(/^\[([^\]]+)\]\((https?:\/\/[^)]+)\)$/);
-  if (markdownMatch) {
-    return <a className={styles.link} key={key} href={markdownMatch[2]} target="_blank" rel="noreferrer">{markdownMatch[1]}</a>;
-  }
-  if (/^https?:\/\//.test(part)) {
-    return <a className={styles.link} key={key} href={part} target="_blank" rel="noreferrer">{part}</a>;
-  }
-  return part;
-}
+const components: Components = {
+  p: ({ children }) => <p className={styles.text}>{children}</p>,
+  a: ({ href, children }) => (
+    <a className={styles.link} href={href} target="_blank" rel="noreferrer">{children}</a>
+  ),
+};
 
 export default function RichTextRenderer({ text }: Readonly<Props>) {
-  return (
-    <>
-      {text.split('\n\n').map((paragraph, pi) => (
-        <p key={pi} className={styles.text}>
-          {paragraph.split('\n').map((line, li, lines) => (
-            <span key={li}>
-              {line.split(INLINE_SPLIT_REGEX).map((part, i) => renderInlinePart(part, i))}
-              {li < lines.length - 1 && <br />}
-            </span>
-          ))}
-        </p>
-      ))}
-    </>
-  );
+  return <Markdown components={components} allowedElements={['p', 'strong', 'em', 'a', 'br']} unwrapDisallowed>{text}</Markdown>;
 }
